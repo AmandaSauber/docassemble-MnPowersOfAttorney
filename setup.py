95088fc1fcd32b0a5069eaa6de3c5688dad250f4
Amanda Sauber <21244564+AmandaSauber@users.noreply.github.com>
2025-10-29 15:26:19 -0500

    Added al_logo to objects block

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -44,18 +44,16 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MnPowersOfAttorney',
-      version='1.0.1',
+      version='1.1.0',
       description=('Powers of Attorney'),
       long_description='# docassemble.MnPowersOfAttorney\r\n\r\nPowers of Attorney\r\n\r\n## Author\r\n\r\nQuinten Steenhuis\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Quinten Steenhuis',
       author_email='qsteenhuis@suffolk.edu',
-      license='The MIT License',
+      license='MIT',
       url='https://www.lawhelpmn.org/self-help-library/legal-resource/power-attorney-do-it-yourself',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
-      install_requires=['docassemble.ALAnyState', 'docassemble.AssemblyLine>=2.24.0'],
+      packages=find_namespace_packages(),
+      install_requires=['docassemble.GithubFeedbackForm @ git+https://github.com/SuffolkLITLab/docassemble-GithubFeedbackForm.git@main'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/MnPowersOfAttorney/', package='docassemble.MnPowersOfAttorney'),
      )
-
